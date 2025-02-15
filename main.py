e4ca050164b9a6f6c22203abc68d40644e951891
Your Name <Matis.van.wassenhove@leerling.de-passer.be>
2025-02-15 16:30:47 +0100

    first commit

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-# Deze file is voor de replit-gebruikers.
-# Plak hier je oplossingen in om ze uit te voeren.
+mensen = int(input("Met hoeveel mensen kom je naar het consert?"))
+prijs = float(input("Wat is de prijs voor 1 ticket?"))
+totaalprijs = mensen * prijs
+print("De totale prijs is",totaalprijs,"euro")
+
+tempCelcius = int(input("Wat is de temperatuur in graden celcius"))
+Fahrenheit = tempCelcius * 1.8 + 32
+print("De themperatuur in graden fahrenheit is",Fahrenheit)
+
+meter = int(input("Geef een afstand in meter"))
+centimeter = meter * 100
+print(meter,"meter is gelijk aan",centimeter,"centimeter")
