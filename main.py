760b6805608a4074a1d2a4a16e644439633c15f2
Your Name <Matis.van.wassenhove@leerling.de-passer.be>
2025-02-22 15:41:57 +0100

    les 2

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-mensen = int(input("Met hoeveel mensen kom je naar het consert?"))
-prijs = float(input("Wat is de prijs voor 1 ticket?"))
-totaalprijs = mensen * prijs
-print("De totale prijs is",totaalprijs,"euro")
+def vriend(naam):
+    print("Hallo",naam,"leuk je te zien")
+vriend("Bob")
 
-tempCelcius = int(input("Wat is de temperatuur in graden celcius"))
-Fahrenheit = tempCelcius * 1.8 + 32
-print("De themperatuur in graden fahrenheit is",Fahrenheit)
-
-meter = int(input("Geef een afstand in meter"))
-centimeter = meter * 100
-print(meter,"meter is gelijk aan",centimeter,"centimeter")
+def papegaai(getal):
+    return getal * 3
+print(papegaai("hoi"))
