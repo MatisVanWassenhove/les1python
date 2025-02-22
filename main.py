535d197b81e19b441c2bb8c67d6be1ab94679923
Your Name <Matis.van.wassenhove@leerling.de-passer.be>
2025-02-22 15:49:39 +0100

    les 2 oef 2

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ vriend("Bob")
 def papegaai(getal):
     return getal * 3
 print(papegaai("hoi"))
+
+def vraag_naam_leeftijd():
+    naam = input("Wat is je naam?")
+    leeftijd = int(input("Wat is je leeftijd"))
+    print("Je naam is", naam, "en je leeftijd is ", leeftijd)
+vraag_naam_leeftijd()
