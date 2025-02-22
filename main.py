88824efbf89e4c757dd9467d5b3bbd3af74660b9
Your Name <Matis.van.wassenhove@leerling.de-passer.be>
2025-02-22 16:29:41 +0100

    les 2 oef 2

--- main.py
+++ main.py
@@ -10,4 +10,18 @@ def vraag_naam_leeftijd():
     naam = input("Wat is je naam?")
     leeftijd = int(input("Wat is je leeftijd"))
     print("Je naam is", naam, "en je leeftijd is ", leeftijd)
-vraag_naam_leeftijd()
+#vraag_naam_leeftijd()
+
+def vraag_input(onderwerp):
+    onderwerp = input("Geef me een onderwerp")
+    return onderwerp
+def print_verhaal(naam,dier,plaats):
+    print("er was eens een man met de naam",naam,"zijn lievelings dier was",dier,"en hij woonde in",plaats,)
+def verhaal_generator():
+    print("Welkom bij de verhaalgenerator")
+    naam = vraag_input("naam")
+    dier = vraag_input("dier")
+    plaats = vraag_input("plaats")
+    print(print_verhaal(naam,dier,plaats))
+    print("Het einde van het verhaal")
+verhaal_generator()
